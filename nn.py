55acc7ab9a3abddb0a653416da001a88f41e0de6
Ivan <ivanwyf@foxmail.com>
2020-02-27 23:35:32 +0800

    add crossover and mutation function

--- nn.py
+++ nn.py
@@ -6,12 +6,14 @@ def sigmoid(x):
     # sigmoid function: f(x) = 1 / (1 + e^(-x))
     return 1/(1+np.exp(-x))
 
+
 def deriv_sigmoid(x):
     # derivative of simgoid function: f'(x) = f(x) * (1 - f(x))
-    fx=sigmoid(x)
+    fx = sigmoid(x)
     return fx*(1-fx)
 
-def mse_loss(y_true,y_predict):
+
+def mse_loss(y_true, y_predict):
     # loss function
     return ((y_true-y_predict)**2).mean()
 
@@ -20,17 +22,18 @@ class Neuron:
     """
     single neuron class
     """
-    def __init__(self,weights,bias):
-        self.weights=weights
-        self.bias=bias
 
-    def feedforward(self,inputs):
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
         '''
         output=0
         for i in range(len(inputs)):
             output+=inputs[i]*weights[i]
         '''
-        output=np.dot(self.weights,inputs)+self.bias
+        output = np.dot(self.weights, inputs)+self.bias
         return sigmoid(output)
 
 
@@ -44,40 +47,49 @@ class NeuralNetwork:
           3             5            2            
     └─────────────┴──────────────┴──────────────┘
     """
+
     def __init__(self):
         # hidden layer neurons's initial weights
-        hidden_weights=[0,1,0]
+        hidden_weights = [0, 1, 0]
         # output layer neurons's initial weights
-        output_weights=[0,1,0,1,0]
-        # all neurons's initial bias
-        bias=0
+        output_weights = [0, 1, 0, 1, 0]
+        # all neurons's initial bias: 0
+        bias = 0
 
         # hidden layer neurons
-        self.h1=Neuron(hidden_weights,bias)
-        self.h2=Neuron(hidden_weights,bias)
-        self.h3=Neuron(hidden_weights,bias)
-        self.h4=Neuron(hidden_weights,bias)
-        self.h5=Neuron(hidden_weights,bias)
+        self.h1 = Neuron(hidden_weights, bias)
+        self.h2 = Neuron(hidden_weights, bias)
+        self.h3 = Neuron(hidden_weights, bias)
+        self.h4 = Neuron(hidden_weights, bias)
+        self.h5 = Neuron(hidden_weights, bias)
+        self.hs = [self.h1, self.h2, self.h3, self.h4, self.h5]
 
         # output layer neurons
-        self.o1=Neuron(output_weights,bias)
-        self.o2=Neuron(output_weights,bias)
+        self.o1 = Neuron(output_weights, bias)
+        self.o2 = Neuron(output_weights, bias)
+        self.os = [self.o1, self.o2]
 
-    def feedforward(self,inputs):
-        output_h1=self.h1.feedforward(inputs)
-        output_h2=self.h2.feedforward(inputs)
-        output_h3=self.h3.feedforward(inputs)
-        output_h4=self.h4.feedforward(inputs)
-        output_h5=self.h5.feedforward(inputs)
-        
-        output_h=[output_h1,output_h2,output_h3,output_h4,output_h5]
+        # network's fitness score
+        self.score = 0
 
-        output_o1=self.o1.feedforward(output_h)
-        output_o2=self.o2.feedforward(output_h)
+        # network's accum_rate calculated by all networks' score
+        self.accum_rate = 0
 
-        return [output_o1,output_o2]
+    def feedforward(self, inputs):
+        output_h1 = self.h1.feedforward(inputs)
+        output_h2 = self.h2.feedforward(inputs)
+        output_h3 = self.h3.feedforward(inputs)
+        output_h4 = self.h4.feedforward(inputs)
+        output_h5 = self.h5.feedforward(inputs)
 
-    def train(self,x_train,y_train):
+        output_h = [output_h1, output_h2, output_h3, output_h4, output_h5]
+
+        output_o1 = self.o1.feedforward(output_h)
+        output_o2 = self.o2.feedforward(output_h)
+
+        return [output_o1, output_o2]
+
+    def train(self, x_train, y_train):
         '''
         epochs=1000  # train loop times
         lr=0.1  # learning rate
@@ -133,81 +145,123 @@ class NeuralNetwork:
                 h5=sigmoid(sum_h5)
         '''
 
-def sort_score(nets):
+
+def sort_score(nets) -> list:
     """
     sort networks by their score, big<-->small
     """
     for i in range(len(nets)-1):
-        for j in range(i+1,len(nets)):
-            if nets[i][1]<nets[j][1]:
+        for j in range(i+1, len(nets)):
+            if nets[i].score < nets[j].score:
                 # swap
-                temp=nets[i]
-                nets[i]=nets[j]
-                nets[j]=temp
+                temp = nets[i]
+                nets[i] = nets[j]
+                nets[j] = temp
     return nets
 
 
-def update_score(nets):
+def update_score(nets) -> list:
     """
     update every network's score by fitness per loop
     fitness is determined by every networks's behaves in the game
     """
     for i in range(len(nets)):
-        nets[i]=(nets[i],i)
+        nets[i].score = i
     return nets
 
 
-def roulette_select(nets):
+def roulette_selection(nets) -> NeuralNetwork:
     """
-    select one network from all networks by using 'roulette' algorithm
+    select one network from all networks by using 'roullette wheel selection' algorithm
     roulette algorithm tend to select one network that has higher score
     """
     # calculate all networks' toatal score
-    total_score=0
+    total_score = 0
     for net in nets:
-        total_score+=net[1]
+        total_score += net.score
 
-    # calculate every network's accum_rate and add it to tuple
-    accum_rate=0
+    # calculate every network's accum_rate
+    accum_rate = 0
     for i in range(len(nets)):
-        rate=nets[i][1]/total_score # one's probability
-        accum_rate+=rate
-        nets[i]=nets[i]+(accum_rate,)
-    
+        rate = nets[i].score/total_score  # one's probability
+        accum_rate += rate
+        nets[i].accum_rate = accum_rate
+
     # select one network randomly by each probability
-    n=random.random()
+    n = random.random()
     for net in nets:
-        if n<net[2]:
+        if n < net.accum_rate:
             return net
-    
+
+
+def crossover(nets) -> NeuralNetwork:
+    """
+    select two networks through 'roullette wheel selection' algorithm,
+    then cross their genes(weights) randomly to get a new child(network)
+    """
+    father = roulette_selection(nets)
+    mother = roulette_selection(nets)
+    child = NeuralNetwork()
+    # cross hidden layer
+    for i in range(len(child.hs)):
+        if random.random() < 0.5:
+            child.hs[i] = father.hs[i]
+        else:
+            child.hs[i] = mother.hs[i]
+
+    # cross output layer
+    for i in range(len(child.os)):
+        if random.random() < 0.5:
+            child.os[i] = father.os[i]
+        else:
+            child.os[i] = mother.os[i]
+
+    return child
+
+
+def mutation(nets) -> list:
+    """
+    mutate children networks by 'Pm'
+    'Pm' is the probability of mutation
+    """
+    pm = 0.1
+    for net in nets:
+        # every hidden layer neuron
+        for h in net.hs:
+            r = random.random()
+            if r < pm:
+                # mutate weights and bias
+                for i in range(len(h.weights)):
+                    h.weights[i] += random.uniform(-0.1, 0.1)*h.weights[i]
+                h.bias += random.uniform(-0.1, 0.1)*h.bias
+    return nets
+
 
 def main():
-    weights=[1,2] # w1=1, w2=2
-    bias=5
-    neuron=Neuron(weights,bias)
-    inputs=[2,3] # x1=2, x2=3
+    weights = [1, 2]  # w1=1, w2=2
+    bias = 5
+    neuron = Neuron(weights, bias)
+    inputs = [2, 3]  # x1=2, x2=3
     print(neuron.feedforward(inputs))
 
-    network=NeuralNetwork()
-    inputs=[2,3,4]
+    network = NeuralNetwork()
+    inputs = [2, 3, 4]
     print(network.feedforward(inputs))
 
-    # network list, every item is a tuple: (network, score)
-    nets=[]
-    # initialize network list, size:100
+    # network list, every item is a NuralNetwork
+    nets = []
+    # initialize network list, size: 100
     for i in range(100):
-        nets.append((NeuralNetwork(),0))
-    
-    nets=update_score(nets)
-    nets=sort_score(nets)
+        nets.append(NeuralNetwork())
+
+    nets = update_score(nets)
+    nets = sort_score(nets)
 
     # get top 20 networks
-    top_twenty=[nets[i] for i in range(20)]
-    print(top_twenty)
+    top_twenty = [nets[i].score for i in range(20)]
 
-    net=roulette_select(nets)
-    print(net)
-
+    net = roulette_selection(nets)
+    print(net.score)
 
 
 main()
